a14d4e86da25b80ddf9b629ede9c1a660da7c925
cassius269 <fahamygaston@gmail.com>
2026-03-28 22:29:57 +0100

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './assets/styles/App.scss'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import BookManager from './components/BookManager'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <main>
+      <h1>Gestion de livre</h1>
+      <BookManager />
+     </main>
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BookManager.jsx
@@ -0,0 +1,8 @@
+function BookManager(){
+
+    return (
+        <h2>Je suis le composant BookManager</h2>
+    )
+}
+
+export default BookManager;
